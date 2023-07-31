7acb371c66ef95a4bdca9680a8dcd5725263db1a
Helmi <helmiariefm@gmail.com>
2023-07-31 11:17:53 +0700

    main layout

--- adm-web-ui/src/App.vue
+++ adm-web-ui/src/App.vue
@@ -2,7 +2,17 @@
 </script>
 
 <template>
-  <div class="text-cyan-800 text-center">
-    <h1>Alfaeeza</h1>
+  <!-- SIDEBAR -->
+  <div class="bg-yellow-700 w-52 h-full fixed">
+  </div>
+
+  <div>
+    <!-- NAVBAR -->
+    <div class="bg-red-700 h-16 ml-52">      
+    </div>
+
+    <!-- MAIN -->
+    <main class="bg-gray-700 w-full h-[91.3vh]">
+    </main>
   </div>
 </template>
