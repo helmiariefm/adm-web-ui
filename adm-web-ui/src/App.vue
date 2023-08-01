9898ef1d83adf4caa9c030447b9d14a915a674c1
Helmi <helmiariefm@gmail.com>
2023-08-01 13:22:01 +0700

    install pinia, make the hide sidebar button work

--- adm-web-ui/src/App.vue
+++ adm-web-ui/src/App.vue
@@ -2,21 +2,26 @@
   import Sidebar from "./organisms/Sidebar.vue"
   import Navbar from "./organisms/Navbar.vue"
   import MainContent from "./organisms/MainContent.vue"
+  import { useCounterStore } from "./stores/counter"
+  import { mapWritableState } from "pinia"
 
   export default{
     components:{
       Sidebar,
       Navbar,
       MainContent
-    }
+    },
+    computed: {
+      ...mapWritableState(useCounterStore, ['isSidebarVisible'])
+    }    
   }
 </script>
 
-<template>  
+<template>
   <Sidebar/>
 
   <div class="flex">
-    <div class="w-full h-full md:ml-60">      
+    <div :class="`w-full h-full ${isSidebarVisible ? 'md:ml-60' : 'md:ml-0'}`">
       <Navbar/>      
       <MainContent/>
     </div>
